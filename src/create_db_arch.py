ae725baaf44efa509281aea5cad7447ebd56b55e
Anna Hernandez <annahernandezduran@gmail.com>
2025-09-22 20:10:46 +0200

    created db creation and data insertoin functions

--- src/create_db_arch.py
+++ src/create_db_arch.py
@@ -1,7 +1,8 @@
 import os
-import psycopg2
+import psycopg
 from dotenv import load_dotenv
 from pgvector.psycopg2 import register_vector
+from psycopg.rows import dict_row
 
 
 load_dotenv()
@@ -10,7 +11,53 @@ load_dotenv()
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
 
-connection = pyscopg2.connect(
-    database=POSTGRES_DB, password=POSTGRES_PASSWORD, host=POSTGRES_HOST
-)
+
+def create_cursor():
+
+    # establish connection with database
+    connection = psycopg.connect(
+        dbname=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host=POSTGRES_HOST,
+    )
+    connection.autocommit = True
+    cursor = connection.cursor()
+
+    # install pgvector extension
+    cursor.execute("CREATE EXTENSION IF NOT EXISTS vector")
+
+    # create the table, including embedding column
+    cursor.execute("DROP TABLE IF EXISTS data")
+
+    cursor.execute(
+        """CREATE TABLE data (
+                id text PRIMARY KEY,
+                content text,
+                metadata text,
+                embedding vector(1536));"""
+    )
+
+    return cursor
+
+
+def insert_embeddings(cursor, data_tuples):
+    
+
+    # establish index. there are two at the moment in pgvector
+    # hnsw and ivf flat
+    # specify the index to use and how to calculate the distance
+    cursor.execute("CREATE INDEX ON data USING hnsw (embedding vector_l2_ops);")
+
+    # insert embeddings into table
+    # they need to be numpy arrays
+    for dt in data_tuples:
+        cursor.execute(
+            "INSERT INTO data (id, content, metadata, embedding) VALUES (%s, %s, %s, %s)",
+            dt,
+        )
+
+def query_db():
+    cursor = 
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 import os
 from uuid import uuid4
 from datetime import datetime
+from create_db_arch import insert_embeddings, create_cursor
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -30,14 +31,21 @@ def get_embedding(text_to_embed):
 
 
 def format_content(text, embedding):
-    return {
-        "id": str(uuid4()),
-        "content": text,
-        "metadata": {
-            "created_at": datetime.now().isoformat(),
-        },
-        "embedding": embedding,
-    }
+    # return {
+    #     "id": str(uuid4()),
+    #     "content": text,
+    #     "metadata": {
+    #         "created_at": datetime.now().isoformat(),
+    #     },
+    #     "embedding": embedding,
+    # }
+
+    return (
+        str(uuid4()),
+        text,
+        datetime.now().isoformat(),
+        embedding,
+    )
 
 
 if __name__ == "__main__":
@@ -45,4 +53,5 @@ if __name__ == "__main__":
     for text in data:
         embedding = get_embedding(text)
         formatted_content = format_content(text, embedding)
-        print(formatted_content)
+        cursor = create_cursor()
+        insert_embeddings([formatted_content])
